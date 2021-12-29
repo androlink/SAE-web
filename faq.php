bf3b2b02c1450b9fd1a61edb8aa6ac86a048078f
Sigma <59624364+androlink@users.noreply.github.com>
2021-12-29 10:24:13 +0100

    "remplissage des paragraphes d'information" et <ajout de balise section>

--- faq.php
+++ faq.php
@@ -51,14 +51,14 @@
                             <section>
                                 <details>
                                     <summary>Dans les matières info? </summary>
-                                    <p>Vous aurez environs 18h par semaines de cours d'informatiques ( Algorithmie, web, langages de programmation, Base de Donnée, archi, OS) </p>
+                                    <p>Vous aurez environs 18h par semaines de cours d'informatiques (Algorithmie, web, langages de programmation, Base de Donnée, archi, OS) </p>
                                 </details>
                                 <details>
                                     <summary> Dans les matières hors info? </summary>
-                                    <p>Vous aurez environs 11h par semaines de cours ayant des liens lointains avec l'informatique ( communication, économie, mathématiques, anglais...) </p>
+                                    <p>Vous aurez environs 11h par semaines de cours ayant des liens lointains avec l'informatique (communication, économie, mathématiques, anglais...) </p>
                                 </details>
                                 <details>
-                                    <summary> Quels sont les horaire de travail? </summary>
+                                    <summary> Quels sont les horaires de travail? </summary>
                                     <p> Vous pourrez avoir cours entre 8h et 18h avec une pause de 12h a 14h sauf le vendredi de 12h à13h30</p>
                                 </details>
                             </section>
@@ -69,7 +69,7 @@
                         </details>
                         <details>
                             <summary> Quel est la compo type d'une promo d'IUT INFO?</summary>
-                            <p></p>
+                            <p> la composition est similaire à une classe de STI2D, peu de filles beaucoup de mec (environ 10% de fille avec 3 filles par classes) en espérant que cela évolue dans le future</p>
                         </details>
                         <details>
                             <summary> Devons nous exercer des stages pendant BUT INFO?</summary>
@@ -87,20 +87,24 @@
                                 <details>
                                     <summary> Où sont les lieux de restauration?</summary>
                                     <section>
-                                        <p> Le studio est une des cafeterias du crous, la plus proche(en face de l'IUT au niveau de l’arrêt de tramway IUT Feyssine). les cafeterias du crous vous propose un menus étudiant composé d'un sandwich, d'un yaourt et d'un dessert pour le même prix que le menus du restaurant universitaire. vous pouvez également prendre un plat chaud ou plein d'autre choses (ça ressemble a une supérette). nous vous conseillons de manger ailleurs car les prix sont élevés.</p>
-                                        <p> L’Archimède est aussi une cafèt du crous,en face de l’arrêt de tram INSA EINSTEIN, mais vous permet d'avoir, avec le menu étudiant, des sandwich de meilleur qualité qu'au studio.</p>
+                                        <p> Le studio est une des cafeterias du crous, la plus proche(en face de l'IUT au niveau de l’arrêt de tramway IUT Feyssine). les cafeterias du crous vous propose un menu étudiant composé d'un sandwich, d'un yaourt et d'un dessert pour le même prix que le menu du restaurant universitaire. Vous pouvez également prendre un plat chaud ou plein d'autres choses (ça ressemble a une supérette). nous vous conseillons de manger ailleurs car les prix sont élevés.</p>
+                                        <p> L’Archimède est aussi une cafèt du crous,en face de l’arrêt de tram INSA EINSTEIN, mais vous permet d'avoir, avec le menu étudiant, des sandwiches de meilleure qualité qu'au studio.</p>
                                         <p> Le restau U "Jussieux" est sur l'avenue Einstein, à deux min à pieds de l’arrêt de tram du même nom. Au restaurant universitaire, vous avez un menu étudiant de six points pour 3,30€ et 1€ pour les boursiers. Une entrée est à un ou deux points, un plat chaud est à 4 points et les desserts sont entre 1 et 3 points. Si vous dépassez les 6 points, chaque points supplémentaires coûtent 55 centimes.</p>
                                         <p> Vous pouvez également amener vos repas et manger dans les locaux de l'iut ou manger dans les nombreux restaurants situés aux alentours.</p>
-                                        <p>!!!Nous vous rappelons que vous disposerez de 2h pour manger sauf le vendredi d'1h30!!!</p>
+                                        <p> !!!Nous vous rappelons que vous disposerez de 2h pour manger sauf le vendredi d'1h30!!!</p>
                                     </section>
                                 </details>
                                 <details>
                                     <summary>Quelles est l'accessibilité aux activités sportives autour de l'IUT?</summary>
-                                    <p></p>
+                                    <section>
+                                    <p> les horaires sont un peu contraignante il y a peu d'activité sportive d'accessible dû aux cours qui finisse tard</p>
+                                        </section>
                                 </details>
                                 <details>
                                     <summary>Quelles est l'accessibilité aux activités culturelles autour de l'IUT?</summary>
-                                    <p></p>
+                                    <section>
+                                        <p> on accés à deux bibliothèques qui possède pas mal d'ouvrage</p>
+                                    </section>
                                 </details>
                             </section>
                         </details>
@@ -112,20 +116,31 @@
                         <section>
                             <details>
                                 <summary>Quels sont les coûts de la formation</summary>
-                                <p><p>
+                                <section>
+                                    <p> il faut compter environ 172€ pour la formation (obligatoire)<p>
+                                    <p> 90€ pour l'abonnement du CROUS (obligatoire)</p>
+
+                                </section>
                             </details>
                             <details>
                                 <summary>Quels sont les coûts des logements autour du campus?</summary>
-                                <p><p>
+                                <section>
+                                    <p> Résidences publiques du Crous : 150€ à 250€ (chambre) </p>
+                                    <p> Résidence étudiante privée : 500€ à 950€ (studio meublé), 480€ (chambre en colocation)</p>
+                                    <p> Location entre particuliers : à partir de 500€ (studio meublé), min 500€ (chambre en colocation)</p>
+
+                                </section>
                             </details>
                             <details>
                                 <summary>Quels sont les prix des transports sur lyon?</summary>
-                                <p><p>
+                                <section>
+                                    <p> 10€ pour un abonnement aux services TCL(tram, metro et bus)</p>
+                                </section>
                             </details>
                         </section>
                     </details>
                     <details>
-                        <summary>Quels sont les service de santé sur le site de la DOUA?</summary>
+                        <summary>Quels sont les services de santé sur le site de la DOUA?</summary>
                         <p><p>
                     </details>
                     <details>
@@ -133,11 +148,14 @@
                         <section>
                             <details>
                                 <summary>Quels sont les magasins à proximité du site de la DOUA?</summary>
-                                <p><p>
+                                <section>
+                                    <p> on peut retrouver un carfour a environ 15min de l'IUT <p>
+                                    <p> il y a aussi un vival a 25min <p>
+                                </section>
                             </details>
                             <details>
                                 <summary>Quels transports desservent l'IUT?</summary>
-                                <p><p>
+                                <p> pour les transports on a le tram T1 qui part de l'IUT Feyssine et qui pars en direction de gratte-ciel en passant par l'arrêt "INSA Einstein"<p>
                             </details>
                         </section>
                     </details>
