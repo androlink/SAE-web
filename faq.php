baf6ae1b59a2c92c1990b19b1515db415a18b4ac
Sigma <59624364+androlink@users.noreply.github.com>
2021-12-30 17:29:32 +0100

    supression de la plus par des fautes

--- faq.php
+++ faq.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <title>acceuil</title>
+    <title>accueil</title>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="style/style_faq.css">
     <link rel="stylesheet" href="style/style_import.css">
@@ -114,22 +114,22 @@
                 <section id="TRAVAIL">  <!--partie questions générales-->
                     <section>
                         <details id='Prereki'>
-                            <summary> Y a t'il des pré-requis pour avoir accès au BUT INFO et pour ne pas avoir trop de difficultés?</summary>
+                            <summary> Y a-t-il des prérequis pour avoir accès au BUT INFO et pour ne pas avoir trop de difficultés?</summary>
                             <article>
-                                <p>Il est possible de candidater à l'aide d'un baccalauréat générales, un baccalauréat technologique STI2D, avec n'importe quel type de baccalauréat professionnels ou bien avec un équivalent du bac. Chaque candidature est évaluée.</p>
-                                <h3>Spécialités du Bac générale :</h3>
+                                <p>Il est possible de candidater à l'aide d'un baccalauréat général, un baccalauréat technologique STI2D, avec n'importe quel type de baccalauréat professionnel ou bien avec un équivalent du bac. Chaque candidature est évaluée.</p>
+                                <h3>Spécialités du Bac général :</h3>
                                 <section>
                                     <div>
                                         <ul>
                                             <li>Arts</li>
                                             <li>Biologie écologie (Lyc. Agri)</li>
                                             <li>Histoire Géo Géopolitique et Sc.Politiques</li>
-                                            <li>Humanités litérature et philosophie</li>
+                                            <li>Humanités literature et philosophie</li>
                                             <li>Langue littérature et cultures étrangères</li>
                                             <li>littérature langues et cultures de l'antiquité</li>
                                             <li>mathématiques</li>
                                             <li>Numérique et Sciences de l'informatiques</li>
-                                            <li>Pysique Chimie</li>
+                                            <li>Physique Chimie</li>
                                             <li>Sciences de la vie et de la terre</li>
                                             <li>Sciences de l'ingénieur</li>
                                             <li>Sciences économiques et sociales</li>
@@ -152,7 +152,7 @@
                                         </ul>
                                     </div>
                                 </section>
-                                <p>Même si ce tableau vous dit quels sont les spécialités les plus adaptés, nous vous conseillons fortement de faire les spécialités Mathématiques et Numériques et Sciences de l'Informatiques, car en plus d'augmenter vos chances d'être selectionner, vous retrouverez ces matières. Il est donc avantageux d'avoir déjà un bagage en arrivant.</p>
+                                <p>Même si ce tableau vous dit quels sont les spécialités les plus adaptés, nous vous conseillons fortement de faire les spécialités Mathématiques et Numériques et Sciences de l'Informatique, car en plus d'augmenter vos chances d'être sélectionné, vous retrouverez ces matières. Il est donc avantageux d'avoir déjà un bagage en arrivant.</p>
                             </article>
                         </details>
                         <details>
@@ -160,25 +160,25 @@
                             <section>
                                 <details>
                                     <summary>Dans les matières "informatiques"? </summary>
-                                    <p>Au premier semestre vous aurez environs 18h par semaines de cours d'informatiques ( Algorithmie, web, langages de programmation, Base de Donnée (modélisation et SQL), archi, OS) </p>
+                                    <p>Au premier semestre vous aurez environs 18h par semaines de cours d'informatiques (Algorithmie, web, langages de programmation, Base de Donnée (modélisation et SQL), archi, OS) </p>
                                 </details>
                                 <details>
                                     <summary> Dans les matières hors "informatique"? </summary>
-                                    <p>Vous aurez environs 11h par semaines de cours ayant des liens lointains avec l'informatique ( communication, économie, mathématiques, anglais...) </p>
+                                    <p>Vous aurez environs 11h par semaines de cours ayant des liens lointains avec l'informatique (communication, économie, mathématiques, anglais...) </p>
                                 </details>
                                 <details>
-                                    <summary> Quels sont les horaire de travail? </summary>
-                                    <p> Vous pourrez avoir cours entre 8h et 18h avec une pause de 12h a 14h sauf le vendredi de 12h à13h30. La plus part du temps vous n'aurez pas cours le jeudi après-midi. Il n'y a aucun cours à l'IUT le samedi.</p>
+                                    <summary> Quels sont les horaires de travail? </summary>
+                                    <p> Vous pourrez avoir cours entre 8h et 18h avec une pause de 12h a 14h sauf le vendredi de 12h à13h30. La plupart du temps, vous n'aurez pas cours le jeudi après-midi. Il n'y a aucun cours à l'IUT le samedi.</p>
                                 </details>
                             </section>
                         </details>
                         <details>
                             <summary id="apres"> Quelles sont les possibles poursuites d’études et insertion dans le monde du travail?</summary>
-                            <p>Après un BUT il est possible d'entrer directement dans le monde du travail. Vous pouvez aussi à la fin de votre seconde année aller en école d'ingénieur (sur dossier) ou partir en 3eme année de licence pour faire ensuite un master. A la fin de la troisième année il est aussi possible de réessayer d'entrer dans une école d'ingénieur, ou de partir directement sur un master sans passer par la licence. A savoir que si vous validez votre deuxième année, même si vous chercher à aller ailleur, vous aurez quand même une place qui vous sera réserver en troisième année pour éviter que vous vous retrouviez sans rien. Si vous souhaitez aller étudier à l'étranger au sein de l'union européenne sachez que vous obtenez 30 crédits européens par semestres</p>
+                            <p>Après un BUT il est possible d'entrer directement dans le monde du travail. Vous pouvez aussi à la fin de votre seconde année aller en école d'ingénieur (sur dossier) ou partir en 3eme année de licence pour faire ensuite un master. À la fin de la troisième année, il est aussi possible de réessayer d'entrer dans une école d'ingénieur, ou de partir directement sur un master sans passer par la licence. À savoir que si vous validez votre deuxième année, même si vous chercher à aller ailleurs, vous aurez quand même une place qui vous sera réserver en troisième année pour éviter que vous vous retrouviez sans rien. Si vous souhaitez aller étudier à l'étranger au sein de l'union européenne sachez que vous obtenez 30 crédits européens par semestres</p>
                         </details>
                         <details>
                             <summary id="promo"> Quel est la composition typique d'une promo d'IUT INFO?</summary>
-                            <p>Une promo est d'environs 130 élèves.<br> 52% de bac généraux | 47% de bac technologiques | 1% de bac professionnels <br> Il n'y a que 9% de filles mais leur nombre augmente depuis plusieurs années, alors mesdames n'hésitez pas.</p>
+                            <p>Une promo est d'environs 130 élèves.<br> 52% de bac général | 47% de bac technologique | 1% de bac professionnels <br> Il n'y a que 9% de filles, mais leur nombre augmente depuis plusieurs années, alors mesdames n'hésitez pas.</p>
                         </details>
                         <details>
                             <summary id="stage"> Devons nous exercer des stages pendant BUT INFO?</summary>
@@ -186,7 +186,7 @@
                         </details>
                         <details>
                             <summary id="vacances"> Quelles sont les périodes de vacances, jours fériés et ponts?</summary>
-                            <p>Vous aurez une semaine de vacances à la toussaint, deux semaines à noël, une semaine en février et deux semaine à pâques.</p>
+                            <p>Vous aurez une semaine de vacances à la toussaint, deux semaines à noël, une semaine en février et deux semaines à pâques.</p>
                         </details>
                     </section>
                     <section>   <!--partie question vie etudiante-->
@@ -196,9 +196,9 @@
                                 <details>
                                     <summary> Où sont les lieux de restauration?</summary>
                                     <article>
-                                        <p> Le studio est une des cafeterias du crous, la plus proche(en face de l'IUT au niveau de l’arrêt de tramway IUT Feyssine). les cafeterias du crous vous propose un menus étudiant composé d'un sandwich, d'un yaourt et d'un dessert pour le même prix que le menus du restaurant universitaire. vous pouvez également prendre un plat chaud ou plein d'autre choses (ça ressemble a une supérette). nous vous conseillons de manger ailleurs car les prix sont élevés.</p>
-                                        <p> L’Archimède est aussi une cafèt du crous,en face de l’arrêt de tram INSA EINSTEIN, mais vous permet d'avoir, avec le menu étudiant, des sandwich de meilleur qualité qu'au studio.</p>
-                                        <p> Le restau U "Jussieux" est sur l'avenue Einstein, à deux min à pieds de l’arrêt de tram du même nom. Au restaurant universitaire, vous avez un menu étudiant de six points pour 3,30€ et 1€ pour les boursiers. Une entrée est à un ou deux points, un plat chaud est à 4 points et les desserts sont entre 1 et 3 points. Si vous dépassez les 6 points, chaque points supplémentaires coûtent 55 centimes.</p>
+                                        <p> Le studio est une des cafeterias du crous, la plus proche(en face de l'IUT au niveau de l’arrêt de tramway IUT Feyssine). Les cafeterias du crous vous propose un menu étudiant composé d'un sandwich, d'un yaourt et d'un dessert pour le même prix que le menu du restaurant universitaire. Vous pouvez également prendre un plat chaud ou plein d'autres choses (ça ressemble à une supérette). Nous vous conseillons de manger ailleurs, car les prix sont élevés.</p>
+                                        <p> L’Archimède est aussi une cafèt du crous, en face de l’arrêt de tram INSA EINSTEIN, mais vous permet d'avoir, avec le menu étudiant, des sandwiches de meilleures qualités qu'au studio.</p>
+                                        <p> Le restau U "Jussieux" est sur l'avenue Einstein, à deux min à pied de l’arrêt de tram du même nom. Au restaurant universitaire, vous avez un menu étudiant de six points pour 3,30€ et 1€ pour les boursiers. Une entrée est à un ou deux points, un plat chaud est à 4 points et les desserts sont entre 1 et 3 points. Si vous dépassez les 6 points, chaque point supplémentaires coûtent 55 centimes.</p>
                                         <p> Vous pouvez également amener vos repas et manger dans les locaux de l'iut ou manger dans les nombreux restaurants situés aux alentours.</p>
                                         <p>!!!Nous vous rappelons que vous disposerez de 2h pour manger sauf le vendredi d'1h30!!!</p>
                                     </article>
@@ -212,7 +212,7 @@
                                 <details>
                                     <summary>Quelles est l'accessibilité aux activités culturelles autour de l'IUT?</summary>
                                     <section>
-                                        <p> on accés à deux bibliothèques qui possède pas mal d'ouvrage</p>
+                                        <p> on a accés à deux bibliothèques qui possède pas mal d'ouvrage</p>
                                     </section>
                                 </details>
                             </section>
@@ -249,7 +249,7 @@
                         </section>
                     </details>
                     <details>
-                        <summary id="sante">Quels sont les service de santé sur le site de la DOUA?</summary>
+                        <summary id="sante">Quels sont les services de santé sur le site de la DOUA?</summary>
                         <p><p>
                     </details>
                     <details>
