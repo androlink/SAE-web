2aec5fa4493282cf579214929d1a147ddc5fd9f4
Vincent Chavot--Dambrun <vincent.chavot----dambrun@etu.univ-lyon1.fr>
2021-12-30 09:15:53 +0100

    début du responsiv faq (y a juste le form qui marche pas

--- presentation.php
+++ presentation.php
@@ -23,27 +23,27 @@
                 <p>Un BUT se fait en 6 semestres, ce qui est l'équivalent de 3ans.</p>
                 <p>Former des professionnels de l’informatique, compétents au plan technologique, capables de réaliser et maintenir des logiciels, de gérer des projets de développement informatique, de garantir le bon fonctionnement des réseaux et également d’échanger avec les utilisateurs. Dotés de compétences assez larges, les futurs diplômés administrent des réseaux, des bases de données, gèrent les parcs informatiques, développent des utilitaires métiers, assurent assistance et formation aux utilisateurs de l’informatique. Ils sont capables de s’adapter à l’évolution rapide des techniques dans un domaine en constante évolution.</p>
                 <p>Il existe 3 parcours cependant le programme de la première année est commun à tous les parcours, le choix du parcours ce fait à la fin de la première année. Quel que soit le parcours la troisième année se fait en alternance.</p>
-                <p>
+                <section class="bgmain2">
                     <h4>Les trois parcours existants sont :</h4>
                     <ul>
                         <li>Réalisation d’applications : conception, développement, validation</li>
                         <li>Déploiement d’applications communicantes et sécurisées</li>
                         <li>Administration, gestion et exploitation des données</li>
                     </ul>
-                </p>
-                <div></div> <!--Shéma parcours-->
-                <h3>Réalisation d’applications : conception, développement, validation</h3>
-                <p>Parcours  assez généraliste en informatique, contient plus de math que les autres parcours mais contient moins d'économie/gestion</p>
-                <p>Ce parcours se prête bien à la poursuite d'études :
-                    <ul>
-                        <li>En école d'ingénieur</li>
-                        <li>En master spécialisé</li>
-                    </ul>
-                </p>
-                <h3>Déploiement d’applications communicantes et sécurisées <br> ou Parcours administration système, réseau et sécurité</h3>
-                <p>Ce parcours est le seul à avoir la deuxième année en alternance. Il contient également moins de maths et de développement que les autres parcours.</p>
-                <h3>Administration, gestion et exploitation des données</h3>
-                <p>Ce parcours est plus axé bases de donnée/informatique décisionnelle.</p>
+                    <div></div> <!--Shéma parcours-->
+                    <h3>Réalisation d’applications : conception, développement, validation</h3>
+                    <p>Parcours  assez généraliste en informatique, contient plus de math que les autres parcours mais contient moins d'économie/gestion</p>
+                    <p>Ce parcours se prête bien à la poursuite d'études :
+                        <ul>
+                            <li>En école d'ingénieur</li>
+                            <li>En master spécialisé</li>
+                        </ul>
+                    </p>
+                    <h3>Déploiement d’applications communicantes et sécurisées <br> ou Parcours administration système, réseau et sécurité</h3>
+                    <p>Ce parcours est le seul à avoir la deuxième année en alternance. Il contient également moins de maths et de développement que les autres parcours.</p>
+                    <h3>Administration, gestion et exploitation des données</h3>
+                    <p>Ce parcours est plus axé bases de donnée/informatique décisionnelle.</p>
+                </section>
             </article>
             <!--<section>
                 <h1>5 Bonnes Raisons de faire un BUT informatique</h1>
